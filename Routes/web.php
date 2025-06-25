685e41a6b6eebe0c2f68164cdd5ef317988e9981
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-06-25 13:53:24 +0700

    update data products

--- Routes/web.php
+++ Routes/web.php
@@ -41,7 +41,7 @@ Route::middleware(['auth', 'web', 'verified'])->group(function () {
             $sheet = $spreadsheet->getActiveSheet();
             $data = $sheet->toArray();
             foreach ($data as $i => $row) {
-                if ($i == 0 || empty($row[2])) continue;
+                if ($i == 0) continue;
                 $code = $row[0]; 
                 $name = $row[1];
                 $unit = empty($row[2]) ? 'PCS' : $row[2];
